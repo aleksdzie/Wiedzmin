2532d01da85539b5485f32e76dc62b6f5add86f3
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-12-03 15:02:54 +0100

    Wiedzmin hero app

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Wiedzmin postac = new Wiedzmin("Stefan", "wojownik", 20, 20, true);
+        System.out.println(postac.getName());
+        System.out.println(postac.getKlasa());
+        System.out.println(postac.getSila());
+        System.out.println(postac.getMana());
+        System.out.println(postac.getNowy());
+        System.out.println(postac.getMoc());
+    }
+}
